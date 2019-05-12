12857b4a730ebc6b8ca0df091f4f4a0d1607bd8d
Bastien Giegel <bgiegel@gmail.com>
2019-05-13 00:04:16 +0200

    improve error handling

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use reqwest::Response;
 use reqwest::Error;
 use std::io;
 use std::fmt;
+use std::num::ParseIntError;
 
 #[derive(Deserialize, Debug)]
 struct People {
@@ -46,21 +47,25 @@ fn main() -> Result<(), Error> {
         for (i, people) in people_response.results.iter().enumerate() {
             println!("{} - {}", i+1, people.name)
         }
-        let choice_index: usize = read_choice();
-
-        match people_response.results.get(usize::from(choice_index)) {
-            None => println!("Wrong index"),
-            Some(people) => println!("{}", people)
+        match read_choice() {
+            Ok(index) => {
+                match people_response.results.get(usize::from(index)) {
+                    None => println!("Wrong index"),
+                    Some(people) => println!("{}", people)
+                };
+            }
+            Err(_err) => println!("Not a number")
         };
     }
 }
 
-fn read_choice() -> usize {
+fn read_choice() -> Result<usize, ParseIntError> {
     println!("Enter character number : ");
     let mut choice = String::new();
     match io::stdin().read_line(&mut choice) {
         Ok(_n) => {}
-        Err(error) => println!("error: {}", error),
+        Err(error) => panic!(error),
     }
-    choice.trim().parse::<i32>().unwrap() as usize - 1
+
+    choice.trim().parse::<i32>().map(|ch| ch as usize - 1)
 }
